65ff63c05d9796356dc2907f8e9eaa105839b29e
FabyoSilveira <silveira.fabyo@outlook.com>
2024-01-17 21:13:03 -0300

    Spring boot project setup

---
+++ src/main/java/todoapi/TodobackApplication.java
@@ -0,0 +1,13 @@
+package todoapi.todoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodobackApplication.class, args);
+	}
+
+}
